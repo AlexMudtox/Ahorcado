71882eae7f46e570b5ac55f1a24ef8115e5f6a23
alejandro <47414658+AlexMudtox@users.noreply.github.com>
2024-08-05 09:41:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const startButton = document.getElementById('startButton');
+const startGame = () => {
+    usedLetters = [];
+    mistakes = 0;
+    hits = 0;
+    wordContainer.innerHTML = '';
+    usedLettersElement.innerHTML = '';
+    startButton.style.display = 'none';
+    drawHangMan();
+    selectRandomWord();
+    drawWord();
+    document.addEventListener('keydown', letterEvent);
+};
+
+startButton.addEventListener('click', startGame);
