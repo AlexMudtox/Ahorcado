c5fdb8ffd0d55a28a898c609a40418c625984cd9
PCEMI\emili <emiliosansanchez@gmail.com>
2024-08-05 20:56:15 +0200

    añadidas palabras

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 
 
 
-
 const startButton = document.getElementById('startButton');
 const startGame = () => {
     usedLetters = [];
@@ -17,5 +16,25 @@ const startGame = () => {
     drawWord();
     document.addEventListener('keydown', letterEvent);
 };
-
+let palabras = [
+    "Ganador",    
+    "Insulto",
+    "Irrigar",
+    "Flipar",
+    "Evadirse",
+    "Himalaya",
+    "Gelatina",
+    "Aspen",
+    "Mozos"
+    ]
+    
+    let respuesta = "";
+    let maxWrong = 6;
+    let errores = 0;
+    let acertadas = [];
+    
+    function randomWord() {
+        respuesta = palabras[Math.floor(Math.random() *palabras.length)];
+    
+    }
 startButton.addEventListener('click', startGame);
